97899d6e74ffdf3eea8cce636e060eb892316206
Figu404 <Filip.h.strom@Gmail.com>
2019-10-12 15:13:27 +0200

    Fixat Navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.css';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <ul>
+        <li>
+          loga
+        </li>
+        <li>
+          nånting
+        </li>
+        <li>
+          hejdå!
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
